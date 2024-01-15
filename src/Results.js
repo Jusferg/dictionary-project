a898ae5b1036354e7b12b88a178c2d6ff56e8cd7
Tia <90842451+Jusferg@users.noreply.github.com>
2024-01-14 20:03:12 -0500

    added phonetics component and tested audio

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,29 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
     if (props.definitions) {
-    return <div className="Results">
-        <h2>{props.definitions.word}</h2>
+    return (
+        <div className="Results">
+            <h2>{props.definitions.word}</h2>
+        {props.definitions.phonetics.map(function(phonetics, index) {
+            return (
+            <div key="{index}">
+                    <Phonetics phonetics={phonetics} />
+            </div>
+            );
+            })}  
+
         {props.definitions.meanings.map(function(meaning, index) {
-            return <div key="{index}">
-                <Meaning meaning={meaning} />
-            </div>;
+            return (
+            <div key="{index}">
+                    <Meaning meaning={meaning} />
+            </div>
+            );
         })}
-           </div>;
+        </div>
+    );
     } else {
         return null;
     }
